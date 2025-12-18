cab3ffce49f4e8a43225edf0603474968a2b9e66
bashar <ninjastackx@gmail.com>
2025-12-19 02:30:25 +0300

    read operration in admin dashboard

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,32 @@
+import { prisma } from "@/lib/prisma";
+import { useUser } from "@/lib/useUser";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const page = () => {
-  return <div className="">Admin Page</div>;
+const page = async () => {
+  const { user } = await useUser();
+  if (!user || user.role !== "ADMIN") {
+    redirect("/");
+  }
+  const users = await prisma.user.findMany();
+  const comments = await prisma.comment.findMany();
+  const posts = await prisma.article.findMany();
+  return (
+    <>
+      Users:
+      {users?.map((usr) => (
+        <div key={usr.id}>
+          <h2>{usr.name}</h2>
+          <p>{usr.email}</p>
+          <hr />
+        </div>
+      ))}
+      <div className="bg-blue-300 rounded-2xl flex justify-center items-center gap-2.5 p-1.5 m-2">
+        <h3>Total Comments: {comments.length}</h3>|
+        <h3>Total Posts: {posts.length}</h3>
+      </div>
+    </>
+  );
 };
 
 export default page;
